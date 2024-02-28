8604a8dece07618266656148b16a4f9f69da2d27
Ethan Che <ethanche1@gmail.com>
2024-02-27 19:57:00 -0500

    Server receives message to start game manually

--- src/rooms/Lobby.js
+++ src/rooms/Lobby.js
@@ -7,10 +7,10 @@ exports.Lobby = class extends colyseus.Room {
   onCreate (options) {
     this.setState(new LobbyState());
 
-    this.onMessage("type", (client, message) => {
-      //
-      // handle "type" message.
-      //
+    this.onMessage("startGame", (client, message) => { //If we get a message to manually start the game from any client
+      // Tell all clients to switch rooms
+      this.broadcast("switchRoom", "MyRoom"); // Replace "MyRoom" with the game room name when it is made
+      console.log(message);
     });
 
   }
@@ -20,6 +20,7 @@ exports.Lobby = class extends colyseus.Room {
     console.log(`${this.clients.length} players in lobby.`);
     if (this.clients.length == this.maxClients) { // If all players join, move them to the game room.
         this.broadcast("switchRoom", "MyRoom"); // Replace "MyRoom" with the game room name when it is made
+        // This is telling the clients to switch rooms, to the room "MyRoom". The logic behind this will have to be implemented.
     }
   }
 
